56a04a18f01ff8eca71c10ed028cfb18bcf75bcb
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2019-11-06 14:15:28 +0000

    Kata4 fin

---
+++ src/kata4/view/HistogramDisplay.java
@@ -0,0 +1,56 @@
+package kata4.view;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import kata4.model.Histogram;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame{
+    
+    private final Histogram<String> histogram;
+    
+    public HistogramDisplay( Histogram<String> histogram) {
+        super("HISTOGRAMA");
+        this.histogram = histogram;
+        this.setContentPane(createPanel());
+        this.pack();
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(500,400));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataset){
+        JFreeChart chart = ChartFactory.createBarChart3D("Histograma JFreChart", 
+                                                         "Dominios email",
+                                                         "Nº de emails", 
+                                                         dataset, 
+                                                         PlotOrientation.VERTICAL,
+                                                         false, 
+                                                         false, 
+                                                         rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    public void execute(){
+        setVisible(true);
+    }
+
+    private DefaultCategoryDataset createDataset() {
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        for (String key: histogram.keySet()) {
+            dataSet.addValue(histogram.get(key),"",key);
+        }
+        return dataSet;
+    }
+    
+    
+}
---
+++ src/kata4/view/MailListReader.java
@@ -0,0 +1,31 @@
+package kata4.view;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import kata4.model.Mail;
+
+public class MailListReader {
+    
+    public static List<Mail> read (String fileName){
+        List<Mail> list = new ArrayList<>();
+        
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            IteratorReader iteratorReader = new IteratorReader(reader);
+            
+            for (String line : iteratorReader) 
+                if(Mail.isMail(line))
+                    list.add(new Mail(line));
+            
+        }catch(FileNotFoundException exception){
+            System.out.println("ERROR MailLisReader::read (FileNotFoundException)" + exception.getLocalizedMessage());
+        }catch(IOException exception){
+            System.out.println("ERROR MailLisReader::read (IOException)" + exception.getLocalizedMessage());
+        }
+        return list;
+    }
+}
