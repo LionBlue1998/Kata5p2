8f92ebb34e590d2d02f8b077387f0961101d7ea8
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2020-01-24 01:39:31 +0000

    Kata5p2: V2

--- src/kata5/main/Kata5p2.java
+++ src/kata5/main/Kata5p2.java
@@ -1,19 +1,21 @@
 package kata5.main;
 
+import java.util.ArrayList;
 import java.util.List;
 import kata5.model.Histogram;
 import kata5.model.Mail;
 import kata5.view.MailHistogramBuilder;
 import kata5.view.HistogramDisplay;
 import kata5.view.MailListReader;
+import kata5.view.MailListReaderBD;
 
-public class Kata4 { // controlador
+public class Kata5p2 { // controlador
 
     public static void main(String[] args) {
-        String fileName = new String("email.txt");
+        String fileName = "email.txt";
         List<Mail> mailList = MailListReader.read(fileName);
         Histogram histogram = MailHistogramBuilder.build(mailList);
-        
+        ArrayList<String> mails = MailListReaderBD.read(fileName);
         HistogramDisplay histo = new HistogramDisplay(histogram);
         histo.execute();
     }
